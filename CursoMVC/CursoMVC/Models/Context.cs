97ae669eb77891769382edd0474c9feff07be82f
Luiz Chequini <luizchequini@gmail.com>
2021-03-04 14:56:51 -0300

    Criando a primeira aplicação ASP,NET - Criação Model Categoria e Migração

---
+++ CursoMVC/CursoMVC/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-CV9KCCO0\\SQLEXPRESS;Database=BootcampLocalizaMrvCursoMvc;Trusted_Connection=True;MultipleActiveResultSets=true");
+            
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
